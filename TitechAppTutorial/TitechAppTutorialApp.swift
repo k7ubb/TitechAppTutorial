6cedef78f38657d73cbf61885092492bb21339c1
k7ubb <kubota.sou@vega.ocn.ne.jp>
2021-05-23 10:55:31 +0900

    Initial Commit

---
+++ TitechAppTutorial/TitechAppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitechAppTutorialApp.swift
+//  TitechAppTutorial
+//
+//  Created by 久保田聡 on 2021/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TitechAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
